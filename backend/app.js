7bc030c9ccab32fb8dd1bd8ff9a15819e9095e0d
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-06-25 17:05:32 +0600

    moedel&route

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 
+const productsRouter = require('./routers/products')
+
 require('dotenv/config');
 const api = process.env.API_URL;
 
@@ -12,42 +14,10 @@ const api = process.env.API_URL;
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
 
-const Product = mongoose.model('Product', productSchema)
 
-app.get(`${api}/products`, async (req, res) => {
-    const productList = await Product.find();
-
-    if (!productList) {
-        res.status(500).json({ success: false })
-    }
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct) => {
-        res.status(201).json(createdProduct)
-    }).catch(err => {
-        res.status(500).json({
-            error: err,
-            success: false,
-        })
-    })
-})
+//Routes
+app.use(`${api}/products`, productsRouter)
 
 //connecting mongoDb: make sure your Ip is added in mongoDb server 
 mongoose.connect(process.env.CONNECTION_STRING,
