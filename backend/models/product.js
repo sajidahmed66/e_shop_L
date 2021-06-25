3f8a30edadd6e86e565b58a5d6e2e3f91b4b3988
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-06-26 03:31:48 +0600

    oders_api

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,11 +1,55 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+        type: String
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
     countInStock: {
         type: Number,
-        required: true
+        required: true,
+        min: 0,
+        max: 255
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    isFetured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
     }
 })
 
