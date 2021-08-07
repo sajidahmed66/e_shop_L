92f535735bbde7c9aa1bb3553cef7820072196b3
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-08-07 12:41:00 +0600

    changing Direction now working on app UI

---
+++ e_shop/app/Screens/Products/ProductContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, ActivityIndicator, Dimensions } from 'react-native'
+
+const ProductContainer = () => {
+    return (
+        <View>
+            <Text>Product Container </Text>
+        </View>
+    )
+}
+
+export default ProductContainer
+
+const styles = StyleSheet.create({})
